a6c5bc3eb0d986bce31d0266b968cd4d4c277cbd
Katharina Sabel <sabel.katharina@gmail.com>
2016-10-23 01:58:31 +0200

    Adding basic recipe renderer

--- cookbook/core/urls.py
+++ cookbook/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^recipe/(?P<id>[0-9]+)/$', views.recipe),
 ]
--- cookbook/core/views.py
+++ cookbook/core/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Recipe
+
+import json
 
 def index(request):
     return HttpResponse("This is where the cookbook index will one day be!")
 
 
+def recipe(request, id):
+  recipe = get_object_or_404(Recipe, pk=id)
+
+  instructions = json.loads(recipe.instructions)
+  ingredients = recipe.ingredients.all()
+
+  return render(request, 'core/recipe.html', 
+    { 
+      'recipe': recipe, 
+      'instructions': instructions, 
+      'ingredients': ingredients
+    })
